c325ceff520d5e4886f997a30481f9877c64fafd
Tim Pease <tim.pease@gmail.com>
2013-04-16 16:47:08 -0600

    adding some more tests

--- test/client/docs_test.rb
+++ test/client/docs_test.rb
@@ -37,24 +37,76 @@ describe Elastomer::Client::Docs do
 
   after do
     @docs.delete_by_query :q => '*:*'
-    @index.flush :refresh => true
+    @index.refresh
   end
 
   it 'gets documents from the search index' do
     h = @docs.get :id => '1', :type => 'doc1'
     assert_equal false, h['exists']
 
-    @docs.add \
-      :_id    => 1,
-      :_type  => 'doc1',
-      :title  => 'just a test document',
-      :author => 'pea53'
+    populate!
 
     h = @docs.get :id => '1', :type => 'doc1'
     assert_equal true, h['exists']
+    assert_equal 'mojombo', h['_source']['author']
   end
 
   it 'gets multiple documents from the search index' do
+    populate!
+
+    h = @docs.multi_get :docs => [
+      { :_id => 1, :_type => 'doc1' },
+      { :_id => 1, :_type => 'doc2' }
+    ]
+    authors = h['docs'].map { |d| d['_source']['author'] }
+    assert_equal %w[mojombo pea53], authors
+
+    h = @docs.multi_get :ids => [2, 1], :_type => 'doc1'
+    authors = h['docs'].map { |d| d['_source']['author'] }
+    assert_equal %w[defunkt mojombo], authors
+
+    h = @index.docs('doc1').multi_get :ids => [1, 2, 3, 4]
+    exists = h['docs'].map { |d| d['exists'] }
+    assert_equal [true, true, false, false], exists
+  end
+
+  it 'deletes documents from the search index' do
+    populate!
+    @docs = @index.docs('doc2')
+
+    h = @docs.multi_get :ids => [1, 2]
+    authors = h['docs'].map { |d| d['_source']['author'] }
+    assert_equal %w[pea53 grantr], authors
+
+    @docs.delete :id => 1
+    h = @docs.multi_get :ids => [1, 2]
+    exists = h['docs'].map { |d| d['exists'] }
+    assert_equal [false, true], exists
+  end
+
+  it 'searches for documents' do
+    h = @docs.search :q => '*:*'
+    assert_equal 0, h['hits']['total']
+
+    populate!
+
+    h = @docs.search :q => '*:*'
+    assert_equal 4, h['hits']['total']
+
+    h = @docs.search :q => '*:*', :type => 'doc1'
+    assert_equal 2, h['hits']['total']
+
+    h = @docs.search({
+      :query => {:match_all => {}},
+      :filter => {:term => {:author => 'defunkt'}}
+    }, :type => %w[doc1 doc2] )
+    assert_equal 1, h['hits']['total']
+
+    hit = h['hits']['hits'].first
+    assert_equal 'the author of resque', hit['_source']['title']
+  end
+
+  def populate!
     @docs.add \
       :_id    => 1,
       :_type  => 'doc1',
@@ -73,23 +125,13 @@ describe Elastomer::Client::Docs do
       :title  => 'the author of logging',
       :author => 'pea53'
 
-    h = @docs.multi_get :docs => [
-      { :_id => 1, :_type => 'doc1' },
-      { :_id => 1, :_type => 'doc2' }
-    ]
-    docs = h['docs'].map { |d| d['_source'] }
-    assert_equal 'mojombo', docs.first['author']
-    assert_equal 'pea53', docs.last['author']
+    @docs.add \
+      :_id    => 2,
+      :_type  => 'doc2',
+      :title  => 'the author of rubber-band',
+      :author => 'grantr'
 
-    h = @docs.multi_get :ids => [2, 1], :_type => 'doc1'
-    docs = h['docs'].map { |d| d['_source'] }
-    assert_equal 'defunkt', docs.first['author']
-    assert_equal 'mojombo', docs.last['author']
-
-    h = @docs.multi_get :ids => [1, 2, 3, 4], :_type => 'doc1'
-    docs = h['docs'].map { |d| d['exists'] }
-    assert_equal [true, true, false, false], docs
+    @index.refresh
   end
-
 
 end

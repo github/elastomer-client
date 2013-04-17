65343b72da4c61d890aa73f4cb7fcaf49ac04b9a
Tim Pease <tim.pease@gmail.com>
2013-04-17 14:24:06 -0600

    adding the first real test for this stuff

--- test/client/bulk_test.rb
+++ test/client/bulk_test.rb
@@ -37,5 +37,48 @@ describe Elastomer::Client::Bulk do
     @index.delete if @index.exists?
   end
 
+  it 'performs a bulk index action' do
+    body = [
+      '{"index" : {"_id":"1", "_type":"tweet"}}',
+      '{"author":"pea53", "message":"just a test tweet"}',
+      '{"index" : {"_id":"1", "_type":"book"}}',
+      '{"author":"John Scalzi", "title":"Old Mans War"}',
+      nil
+    ]
+    body = body.join "\n"
+    h = $client.bulk body, :index => @name
+
+    ok = h['items'].map {|a| a['index']['ok']}
+    assert_equal [true, true], ok
+
+    @index.refresh
+
+    h = @index.docs('tweet').get :id => 1
+    assert_equal 'pea53', h['_source']['author']
+
+    h = @index.docs('book').get :id => 1
+    assert_equal 'John Scalzi', h['_source']['author']
+
+
+    body = [
+      '{"index" : {"_id":"2", "_type":"book"}}',
+      '{"author":"Tolkien", "title":"The Silmarillion"}',
+      '{"delete" : {"_id":"1", "_type":"book"}}',
+      nil
+    ]
+    body = body.join "\n"
+    h = $client.bulk body, :index => @name
+
+    assert h['items'].first['index']['ok'], 'we added a new book'
+    assert h['items'].last['delete']['ok'], 'we removed a book'
+
+    @index.refresh
+
+    h = @index.docs('book').get :id => 1
+    assert !h['exists'], 'was not successfully deleted'
+
+    h = @index.docs('book').get :id => 2
+    assert_equal 'Tolkien', h['_source']['author']
+  end
 
 end

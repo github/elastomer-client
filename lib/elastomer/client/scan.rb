811d411a4d404a63e5b0230307497fa84ead84c4
Tim Pease <tim.pease@gmail.com>
2013-04-18 10:26:22 -0600

    Adding tests for our scan class

--- lib/elastomer/client/scan.rb
+++ lib/elastomer/client/scan.rb
@@ -28,7 +28,27 @@ module Elastomer
 
     class Scan
 
+      # Create a new scan client that can be used to iterate over all the
+      # documents returned by the `query`.
       #
+      # See http://www.elasticsearch.org/guide/reference/api/search/scroll/
+      # and the "Scan" section of http://www.elasticsearch.org/guide/reference/api/search/search-type/
+      #
+      # client - Elastomer::Client used for HTTP requests to the server
+      # query  - The query to scan as a Hash or a JSON encoded String
+      # opts   - Options Hash
+      #   :index  - the name of the index to search
+      #   :type   - the document type to search
+      #   :scroll - the keep alive time of the scrolling request (5 minutes by default)
+      #   :size   - the number of documents per shard to fetch per scroll
+      #
+      # Examples
+      #
+      #   scan = Scan.new(client, {:query => {:match_all => {}}}, :index => 'test-1')
+      #   scan.each_document { |doc|
+      #     doc['_id']
+      #     doc['_source']
+      #   }
       #
       def initialize( client, query, opts = {} )
         @client = client
@@ -44,23 +64,23 @@ module Elastomer
 
       attr_reader :client, :query, :index, :type, :scroll, :size
 
+      # Iterate over all the search results from the scan query.
       #
+      # block  - The block will be called for each set of matching documents
+      #          returned from executing the scan query.
       #
-      def scroll_id
-        return @scroll_id if defined? @scroll_id
-
-        response = client.get '{/index}{/type}/_search',
-                              :search_type => 'scan',
-                              :scroll      => scroll,
-                              :size        => size,
-                              :index       => index,
-                              :type        => type
-
-        @scroll_id = response.body['_scroll_id']
-      end
-
+      # Yields a hits Hash containting the 'total' number of hits, current
+      # 'offset' into that total, and the Array of 'hits' document Hashes.
       #
+      # Examples
       #
+      #   scan.each do |hits|
+      #     hits['total']
+      #     hits['offset']
+      #     hits['hits'].each { |document| ... }
+      #   end
+      #
+      # Returns this Scan instance.
       def each
         loop do
           response = client.get '/_search/scroll', :scroll => scroll, :body => scroll_id
@@ -78,10 +98,40 @@ module Elastomer
         self
       end
 
+      # Iterate over each document from the scan query. This method is just a
+      # convenience wrapper around the `each` method; it iterates the Array of
+      # documents and passes them one by one to the block.
       #
+      # block  - The block will be called for each document returned from
+      #          executing the scan query.
       #
+      # Yields a document Hash.
+      #
+      # Examples
+      #
+      #   scan.each_document do |document|
+      #     document['_id']
+      #     document['_source']
+      #   end
+      #
+      # Returns this Scan instance.
       def each_document( &block )
         each { |hits| hits['hits'].each(&block) }
+      end
+
+      # Internal: Returns the current scroll ID as a String.
+      def scroll_id
+        return @scroll_id if defined? @scroll_id
+
+        response = client.get '{/index}{/type}/_search',
+                              :search_type => 'scan',
+                              :scroll      => scroll,
+                              :size        => size,
+                              :index       => index,
+                              :type        => type,
+                              :body        => query
+
+        @scroll_id = response.body['_scroll_id']
       end
 
     end  # Scan

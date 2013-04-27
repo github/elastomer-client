cb68f1f36cedb19361d472db8bef5258199450f9
Tim Pease <tim.pease@gmail.com>
2013-04-26 18:06:44 -0600

    adding some docos

--- lib/elastomer/notifications.rb
+++ lib/elastomer/notifications.rb
@@ -4,8 +4,38 @@ require 'securerandom'
 require File.expand_path('../client', __FILE__) unless defined? Elastomer::Client
 
 module Elastomer
+
+  # So you want to get notifications from your ElasticSearch client? Well,
+  # you've come to the right place!
+  #
+  #   require 'elastomer/notifications'
+  #
+  # Requiring this module will add ActiveSupport notifications to all
+  # ElasticSearch requests. To subscribe to those requests ...
+  #
+  #   ActiveSupport::Notifications.subscribe('request.client.elastomer') do |name, start_time, end_time, _, payload|
+  #     duration = end_time - start_time
+  #     $stderr.puts '[%s] %s %s (%.3f)' % [payload[:status], payload[:index], payload[:action], duration]
+  #   end
+  #
+  # The payload contains the following bits of information:
+  #
+  # * :index  - index name (if any)
+  # * :type   - documeht type (if any)
+  # * :action - the action being performed
+  # * :url    - request URL
+  # * :method - request method (:head, :get, :put, :post, :delete)
+  # * :status - response status code
+  #
   module Notifications
-    # Internal:
+
+    # The name to subscribe to for notifications
+    NAME = 'request.client.elastomer'.freeze
+
+    # Internal: Execute the given block and provide instrumentaiton info to
+    # subscribers. The name we use for subscriptions is
+    # `request.client.elastomer` and a supplemental payload is provided with
+    # more information about the specific ElasticSearch request.
     #
     # path   - The full request path as a String
     # params - The request params Hash
@@ -36,7 +66,7 @@ module Elastomer
         :action => action
       }
 
-      ActiveSupport::Notifications.instrument('request.client.elastomer', payload) do
+      ActiveSupport::Notifications.instrument(NAME, payload) do
         response = yield
         payload[:method] = response.env[:method]
         payload[:status] = response.status

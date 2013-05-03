4606e61fb7181805972fa6b3889d7a7cd48fc165
Tim Pease <tim.pease@gmail.com>
2013-05-03 13:35:44 -0600

    we are only using the elastomer/client and elastomer/notifications files

--- lib/elastomer.rb
+++
@@ -1,7 +0,0 @@
-# Elastomer provides an interface for indexing documents into a search engine
-# and, subsequently, searching those documents.
-#
-module Elastomer
-end  # Elastomer
-
-require 'elastomer/version'

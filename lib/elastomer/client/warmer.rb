395f1b494e6a479937bdf63c326bfa4c8d915f04
Grant Rodgers <grantr@gmail.com>
2013-05-09 17:51:18 -0700

    add warmer docs

--- lib/elastomer/client/warmer.rb
+++ lib/elastomer/client/warmer.rb
@@ -1,14 +1,24 @@
 module Elastomer
   class Client
 
-
+    # Provides access to warmer API commands.
+    # See http://www.elasticsearch.org/guide/reference/api/admin-indices-warmers/
+    #
+    # index_name  - The name of the index as a String
+    # warmer_name - The name of the warmer as a String
+    #
+    # Returns a Warmer instance.
     def warmer(index_name, warmer_name)
       Warmer.new(self, index_name, warmer_name)
     end
 
-    # XXX docs
     class Warmer
 
+      # Create a new Warmer helper for making warmer API requests.
+      #
+      # client     - Elastomer::Client used for HTTP requests to the server
+      # index_name - The name of the index as a String
+      # name       - The name of the warmer as a String
       def initialize(client, index_name, name)
         raise ArgumentError, 'index name cannot be nil' if index_name.nil?
 
@@ -19,21 +29,45 @@ module Elastomer
 
       attr_reader :client, :index_name, :name
 
-      def create(body, params = {})
-        response = client.put '/{index}{/type}/_warmer/{warmer}', defaults.update(params.update(:body => body))
+      # Create a warmer.
+      # See http://www.elasticsearch.org/guide/reference/api/admin-indices-warmers/
+      #
+      # query  - The query the warmer should run
+      # params - Parameters Hash
+      #
+      # Examples
+      #
+      #   warmer.create(:query => {:match_all => {}})
+      #
+      # Returns the response body as a Hash
+      def create(query, params = {})
+        response = client.put '/{index}{/type}/_warmer/{warmer}', defaults.update(params.update(:body => query))
         response.body
       end
 
+      # Delete a warmer.
+      # See http://www.elasticsearch.org/guide/reference/api/index_/
+      #
+      # params   - Parameters Hash
+      #
+      # Returns the response body as a Hash
       def delete(params = {})
         response = client.delete '/{index}{/type}/_warmer/{warmer}', defaults.update(params)
         response.body
       end
 
+      # Get a warmer.
+      # See http://www.elasticsearch.org/guide/reference/api/index_/
+      #
+      # params   - Parameters Hash
+      #
+      # Returns the response body as a Hash
       def get(params = {})
         response = client.get '/{index}{/type}/_warmer/{warmer}', defaults.update(params)
         response.body
       end
 
+      # Internal: Returns a Hash containing default parameters.
       def defaults
         {:index => index_name, :warmer => name}
       end

93e6ad449f79f9c2182890e78f418691212105a0
Tim Pease <tim.pease@gmail.com>
2013-09-16 13:51:49 -0600

    Use `:read_timeout` instead of the plain `:timeout`
    The cluster health check already has a `timeout` paramter, so we need to use
    the more specific `:read_timeout` instead.

--- lib/elastomer/client.rb
+++ lib/elastomer/client.rb
@@ -16,7 +16,7 @@ module Elastomer
     #   :host - the host as a String
     #   :port - the port number of the server
     #   :url  - the URL as a String (overrides :host and :port)
-    #   :timeout - the timeout in seconds when reading from an HTTP connection
+    #   :read_timeout - the timeout in seconds when reading from an HTTP connection
     #   :open_timeout - the timeout in seconds when opening an HTTP connection
     #   :adapter      - the Faraday adapter to use (defaults to :excon)
     #
@@ -29,13 +29,13 @@ module Elastomer
       @host = uri.host
       @port = uri.port
 
-      @timeout      = opts.fetch :timeout, 5
+      @read_timeout = opts.fetch :read_timeout, 5
       @open_timeout = opts.fetch :open_timeout, 2
       @adapter      = opts.fetch :adapter, :excon
     end
 
     attr_reader :host, :port, :url
-    attr_reader :timeout, :open_timeout
+    attr_reader :read_timeout, :open_timeout
 
     # Returns true if the server is available; returns false otherwise.
     def available?
@@ -58,7 +58,7 @@ module Elastomer
           conn.adapter(*@adapter) :
           conn.adapter(@adapter)
 
-        conn.options[:timeout]      = timeout
+        conn.options[:timeout]      = read_timeout
         conn.options[:open_timeout] = open_timeout
       end
     end
@@ -124,8 +124,8 @@ module Elastomer
     # method - The HTTP method to send [:head, :get, :put, :post, :delete]
     # path   - The path as a String
     # params - Parameters Hash
-    #   :body    - Will be used as the request body
-    #   :timeout - Optional timeout (in seconds) for the request
+    #   :body         - Will be used as the request body
+    #   :read_timeout - Optional read timeout (in seconds) for the request
     #
     # Returns a Faraday::Response
     # Raises an Elastomer::Client::Error on 4XX and 5XX responses
@@ -133,31 +133,31 @@ module Elastomer
       body = params.delete :body
       body = MultiJson.dump body if Hash === body
 
-      timeout = params.delete :timeout
+      read_timeout = params.delete :read_timeout
 
       path = expand_path path, params
 
       response = instrument(path, body, params) do
         case method
         when :head
-          connection.head(path) { |req| req.options[:timeout] = timeout if timeout }
+          connection.head(path) { |req| req.options[:timeout] = read_timeout if read_timeout }
 
         when :get
           connection.get(path) { |req|
             req.body = body if body
-            req.options[:timeout] = timeout if timeout
+            req.options[:timeout] = read_timeout if read_timeout
           }
 
         when :put
-          connection.put(path, body) { |req| req.options[:timeout] = timeout if timeout }
+          connection.put(path, body) { |req| req.options[:timeout] = read_timeout if read_timeout }
 
         when :post
-          connection.post(path, body) { |req| req.options[:timeout] = timeout if timeout }
+          connection.post(path, body) { |req| req.options[:timeout] = read_timeout if read_timeout }
 
         when :delete
           connection.delete(path) { |req|
             req.body = body if body
-            req.options[:timeout] = timeout if timeout
+            req.options[:timeout] = read_timeout if read_timeout
           }
 
         else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,8 +21,8 @@ require 'elastomer/client'
 # the client should always be stateless
 $client_params = {
   :port => ENV['GH_ELASTICSEARCH_PORT'] || 9200,
-  :open_timeout => 1,
-  :read_timeout => 1
+  :read_timeout => 0.5,
+  :open_timeout => 0.5
 }
 $client = Elastomer::Client.new $client_params
 

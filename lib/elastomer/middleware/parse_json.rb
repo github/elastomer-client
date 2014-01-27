fbdb905cf8d73a733d5d8c2483f9d38d674e7c40
Grant Rodgers <grantr@gmail.com>
2014-01-27 11:15:06 -0800

    add json parsing and encoding middleware
    These are simplified versions of the JSON parsers from faraday_middleware.

---
+++ lib/elastomer/middleware/parse_json.rb
@@ -0,0 +1,41 @@
+module Elastomer
+  module Middleware
+
+    # Parse response bodies as JSON.
+    class ParseJson < Faraday::Middleware
+      CONTENT_TYPE = 'Content-Type'.freeze
+      MIME_TYPE    = 'application/json'.freeze
+
+      def call(environment)
+        @app.call(environment).on_complete do |env|
+          if process_response?(env)
+            env[:body] = parse env[:body]
+          end
+        end
+      end
+
+      # Parse the response body.
+      def parse(body)
+        MultiJson.load(body)
+      end
+
+      def process_response?(env)
+        type = response_type(env)
+        has_body?(env) and type == MIME_TYPE
+      end
+
+      def has_body?(env)
+        body = env[:body] and !(body.respond_to?(:to_str) and body.empty?)
+      end
+
+      def response_type(env)
+        type = env[:response_headers][CONTENT_TYPE].to_s
+        type = type.split(';', 2).first if type.index(';')
+        type
+      end
+    end
+  end
+end
+
+Faraday.register_middleware :response,
+  :parse_json => ::Elastomer::Middleware::ParseJson

436fc505ed353b1e9ce9e71575e272c15d1ec8f9
Tim Pease <tim.pease@gmail.com>
2014-03-27 14:44:36 -0600

    a little less happy settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,8 +21,8 @@ require 'elastomer/client'
 # the client should always be stateless
 $client_params = {
   :port => ENV['GH_ELASTICSEARCH_PORT'] || 9200,
-  :read_timeout => 1.0,
-  :open_timeout => 0.5,
+  :read_timeout => 2,
+  :open_timeout => 1,
   :opaque_id => true
 }
 $client = Elastomer::Client.new $client_params

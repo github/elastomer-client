8efafe8add1ec99e23ab769dadae18fd06c20dd1
Tim Pease <tim.pease@gmail.com>
2015-01-21 11:26:52 -0700

    1.4.3 changes this behavior again

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -95,7 +95,8 @@ end
 #
 # Reeturns `true` if the response contains an "alises" key.
 def es_version_always_returns_aliases?
-  $client.semantic_version <= '1.4.0'
+  $client.semantic_version <= '1.4.0' &&
+  $client.semantic_version >= '1.4.3'
 end
 
 # Elasticsearch 1.2 removed support for gateway snapshots.

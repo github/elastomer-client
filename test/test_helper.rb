e023920edbbff0305918877f711bf36826172887
Tim Pease <tim.pease@gmail.com>
2017-03-31 19:53:23 -0600

    removing boxen stuff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,7 +25,7 @@ require "elastomer/client"
 # we are going to use the same client instance everywhere!
 # the client should always be stateless
 $client_params = {
-  :port => ENV["BOXEN_ELASTICSEARCH_PORT"] || 9200,
+  :port => 9200,
   :read_timeout => 10,
   :open_timeout => 1,
   :opaque_id => false

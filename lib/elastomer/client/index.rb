0f2ebe80601dcb9893d21ff45a502e8372812199
Javier Uruen Val <juruen@github.com>
2017-12-05 10:56:39 +0100

    do not use tap and split into two lines instead

--- lib/elastomer/client/index.rb
+++ lib/elastomer/client/index.rb
@@ -230,7 +230,8 @@ module Elastomer
       #
       # Returns the response body as a Hash
       def analyze( text, params = {} )
-        body = params.merge(:text => text.to_s).tap { |h| h.delete(:index) }
+        body = {:text => text.to_s}.merge params
+        body.delete :index
         response = client.get "{/index}/_analyze", update_params(params, :body => body, :action => "index.analyze")
         response.body
       end

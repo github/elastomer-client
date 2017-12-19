59c5bcbadbcc8e1472cdd507ffe2676871dd0316
Eli Reisman <elireisman@github.com>
2017-12-18 16:09:59 -0800

    post-review comment update

--- lib/elastomer/version_support.rb
+++ lib/elastomer/version_support.rb
@@ -71,6 +71,8 @@ module Elastomer
     end
 
     # COMPATIBILITY: return a simple boolean value or legacy {"enabled": true/false}.
+    #
+    # https://www.elastic.co/guide/en/elasticsearch/reference/5.5/breaking_50_mapping_changes.html#_literal_norms_literal
     def strict_boolean(b)
       if es_version_2_x?
         {enabled: b}

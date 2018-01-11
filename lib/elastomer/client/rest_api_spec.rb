4171ec47143d8042fb15f6b6acf8dca2736c5581
Tim Pease <tim.pease@gmail.com>
2018-01-11 09:51:12 -0700

    fixing typos and adding docs

--- lib/elastomer/client/rest_api_spec.rb
+++ lib/elastomer/client/rest_api_spec.rb
@@ -2,9 +2,15 @@
 module Elastomer
   class Client
 
-    # Provides access to the versioned RES API specs for Elasticsearch.
+    # Provides access to the versioned REST API specs for Elasticsearch.
     module RestApiSpec
 
+      # Returns an ApiSpec instance for the given Elasticsearcion version. This
+      # method will load the ApiSpec version class if it has not already been
+      # defined. This prevents bloat by only loading the version specs that are
+      # needed.
+      #
+      # Because of this lazy loading, this method is _not_ thread safe.
       #
       # version - the Elasticsearch version String
       #
@@ -15,8 +21,7 @@ module Elastomer
         self.const_get(classname).new
       end
 
-      # Internal:
-      #
+      # Internal: Load the specific ApiSpec version class for the given version.
       def self.load_api_spec(version)
         path = File.expand_path("../rest_api_spec/api_spec_v#{to_class_version(version)}.rb", __FILE__)
         if File.exists? path
@@ -26,12 +31,11 @@ module Elastomer
         end
       end
 
-      # Internal:
-      #
+      # Internal: Convert a dotted version String into an underscore format
+      # suitable for use in Ruby class names.
       def self.to_class_version(version)
         version.to_s.split(".").slice(0,2).join("_")
       end
-
     end
   end
 end

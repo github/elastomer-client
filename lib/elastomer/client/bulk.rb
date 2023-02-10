354c958ad1836958138d70ed669b9fdf893804f8
Katherine Griffin <42548585+kag728@users.noreply.github.com>
2023-02-10 20:58:15 +0000

    Fix version_support check in bulk class

--- lib/elastomer/client/bulk.rb
+++ lib/elastomer/client/bulk.rb
@@ -40,7 +40,7 @@ module Elastomer
         raise "bulk request body cannot be nil" if body.nil?
         params ||= {}
         updated_params = params.merge(body: body, action: "bulk", rest_api: "bulk")
-        updated_params.delete(:type) if $client.version_support.es_version_8_plus?
+        updated_params.delete(:type) if version_support.es_version_8_plus?
 
         response = self.post "{/index}{/type}/_bulk", updated_params
         response.body

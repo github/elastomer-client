09590bf8aa371698716f565a25c7f34a52046f90
John Hernley <jherns@github.com>
2025-02-07 20:05:38 +0000

    implement follow and auto-follow

---
+++ lib/elastomer_client/client/ccr.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module ElastomerClient
+  class Client
+
+    # Returns a CCR instance
+    def ccr
+      Ccr.new(self)
+    end
+
+    class Ccr
+      # Create a new Ccr for initiating requests for cross cluster replication.
+      # More context: https://www.elastic.co/guide/en/elasticsearch/reference/current/ccr-apis.html
+      #
+      # client     - ElastomerClient::Client used for HTTP requests to the server
+      def initialize(client)
+        @client = client
+      end
+
+      attr_reader :client
+
+      # Creates a new follower index for the provided leader index on the remote cluster.
+      #
+      # follower_index - String name of the follower index to create
+      # body           - Hash of the request body
+      #   :remote_cluster - String name of the remote cluster. Required.
+      #   :leader_index   - String name of the leader index. Required.
+      # params         - Hash of query parameters
+      #
+      # Examples
+      #
+      #   ccr.follow("follower_index", { leader_index: "leader_index", remote_cluster: "leader" })
+      #
+      # See https://www.elastic.co/guide/en/elasticsearch/reference/current/ccr-put-follow.html
+      def follow(follower_index, body, params={})
+        response = client.put "/#{follower_index}/_ccr/follow", params.merge(params, body:, action: "follow", rest_api: "ccr")
+        response.body
+      end
+
+      # Creates a new auto-follow pattern for the provided remote cluster.
+      #
+      # pattern_name - String name of the auto-follow pattern to create
+      # body         - Hash of the request body
+      #   :remote_cluster - String name of the remote cluster. Required.
+      #   :leader_index_patterns   - An array of simple index patterns to match against indices in the remote cluster
+      #   :leader_index_exclusion_patterns    - An array of simple index patterns that can be used to exclude indices from being auto-followed.
+      #   :follow_index_pattern   - The name of follower index. The template {{leader_index}} can be used to derive
+      #                             the name of the follower index from the name of the leader index.
+      # params       - Hash of query parameters
+      #
+      # Examples
+      #
+      #   ccr.auto_follow("follower_pattern", { remote_cluster: "leader", leader_index_patterns: ["leader_index*"]
+      #                                     follow_index_pattern: "{{leader_index}}-follower" })
+      #
+      # See https://www.elastic.co/guide/en/elasticsearch/reference/current/ccr-put-auto-follow-pattern.html
+      def auto_follow(pattern_name, body, params={})
+        response = client.put "/_ccr/auto_follow/#{pattern_name}", params.merge(params, body:, action: "create_auto_follow_pattern", rest_api: "ccr")
+        response.body
+      end
+
+    end
+  end
+end
---
+++ test/client/ccr_test.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+describe ElastomerClient::Client::Ccr do  
+  before do
+    skip "Cannot test Ccr API without a replica cluster" unless $replica_client.available?
+
+    @leader_index = $client.index("leader_index")
+    @follower_index = $replica_client.index("follower_index")
+    if @leader_index.exists?
+      @leader_index.delete
+    end
+    if @follower_index.exists?
+      @follower_index.delete
+    end
+    @leader_index.create(default_index_settings)
+    wait_for_index(@leader_index.name, "green")
+
+    @leader_index.docs.index(document_wrapper("book", { _id: 1, title: "Book 1" }))
+    @leader_index.refresh
+  end
+
+  after do
+    @leader_index.delete if @leader_index.exists?
+    @follower_index.delete if @follower_index.exists?
+  end
+
+  it "successfully follows a leader index" do
+    ccr = $replica_client.ccr
+
+    ccr.follow(@follower_index.name, { leader_index: @leader_index.name, remote_cluster: "leader" })
+    wait_for_index(@follower_index.name, "green")
+    doc = @follower_index.docs.get(id: 1, type: "book")
+
+    assert_equal "Book 1", doc["_source"]["title"]
+  end
+
+  it "successfully implements an auto-follow policy" do
+    ccr = $replica_client.ccr
+
+    ccr.auto_follow("follower_pattern", { remote_cluster: "leader", leader_index_patterns: ["*"], follow_index_pattern: "{{leader_index}}-follower" })
+
+    index = $client.index("followed_index")
+    index.create(default_index_settings)
+    wait_for_index(index.name, "green")
+    follower_index = $replica_client.index("followed_index-follower")
+    wait_for_index(follower_index.name, "green")
+    assert follower_index.exists?
+
+    index.delete
+    follower_index.delete
+  end
+end

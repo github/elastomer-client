5f05d6c249e8139dc662b28b282c8a1ecbadf10a
Sarwaan Ansari <31755174+sarwaan001@users.noreply.github.com>
2025-02-13 09:57:24 +0000

    fix: refactored test

--- test/client/ccr_test.rb
+++ test/client/ccr_test.rb
@@ -16,9 +16,6 @@ describe ElastomerClient::Client::Ccr do
     end
     @leader_index.create(default_index_settings)
     wait_for_index(@leader_index.name, "green")
-
-    @leader_index.docs.index(document_wrapper("book", { _id: 1, title: "Book 1" }))
-    @leader_index.refresh
   end
 
   after :each do
@@ -26,32 +23,45 @@ describe ElastomerClient::Client::Ccr do
     @follower_index.delete if @follower_index.exists?
   end
 
-  it "successfully follows a leader index" do
+  def follow_index(follower_index, leader_index)
     ccr = $replica_client.ccr
+    response = ccr.follow(follower_index.name, { leader_index: leader_index.name, remote_cluster: "leader" })
+    wait_for_index(follower_index.name, "green")
+    response
+  end
 
-    ccr.follow(@follower_index.name, { leader_index: @leader_index.name, remote_cluster: "leader" })
-    wait_for_index(@follower_index.name, "green")
+  def pause_follow(follower_index)
+    ccr = $replica_client.ccr
+    response = ccr.pause_follow(follower_index.name)
+    wait_for_index(follower_index.name, "green")
+    response
+  end
+
+  def create_document(index, type, document)
+    response = index.docs.index(document_wrapper(type, document))
+    index.refresh
+    response
+  end
+
+  it "successfully follows a leader index" do
+    create_document(@leader_index, "book", { _id: 1, title: "Book 1" })
+
+    follow_index(@follower_index, @leader_index)
+
     doc = @follower_index.docs.get(id: 1, type: "book")
 
     assert_equal "Book 1", doc["_source"]["title"]
   end
 
   it "successfully pauses a follower index" do
-    ccr = $replica_client.ccr
+    follow_index(@follower_index, @leader_index)
 
-    ccr.follow(@follower_index.name, { leader_index: @leader_index.name, remote_cluster: "leader" })
-    wait_for_index(@follower_index.name, "green")
-    doc = @follower_index.docs.get(id: 1, type: "book")
-
-    assert_equal "Book 1", doc["_source"]["title"]
-
-    response = ccr.pause_follow(@follower_index.name)
+    response = pause_follow(@follower_index)
 
     assert response["acknowledged"]
 
-    wait_for_index(@follower_index.name, "green")
-    @leader_index.docs.index(document_wrapper("book", { _id: 2, title: "Book 2" }))
-    @leader_index.refresh
+    create_document(@leader_index, "book", { _id: 2, title: "Book 2" })
+
     doc = @follower_index.docs.get(id: 2, type: "book")
 
     refute doc["found"]

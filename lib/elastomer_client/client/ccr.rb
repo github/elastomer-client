91007fce14243cfd03d86a4ea5ab03decaf6a8e2
Sarwaan Ansari <31755174+sarwaan001@users.noreply.github.com>
2025-02-13 10:22:12 +0000

    chore: updated comments to include closed and paused

--- lib/elastomer_client/client/ccr.rb
+++ lib/elastomer_client/client/ccr.rb
@@ -52,6 +52,7 @@ module ElastomerClient
       end
 
       # Unfollows a leader index given a follower index.
+      # The follower index must be paused and closed before unfollowing.
       # 
       # follower_index - String name of the follower index to create
       # params         - Hash of the request body
